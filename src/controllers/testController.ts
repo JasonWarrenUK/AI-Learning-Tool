4ffb916805ad7b1fca3ee71cfc6bfacc76ea5a7d
Jason Warren <contact.jwarren@gmail.com>
2024-05-21 19:44:47 +0100

    Remove invalid/redundant routes & imports - `controllers/`   - `metaController.ts`     - remove `import quizData`   - `testController.ts`     - remove `import quizData`     - remove `getRandomQuestion()` - `routes/`   - `testRoutes.ts`     - remove `/random`

--- src/controllers/testController.ts
+++ src/controllers/testController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import quizData from '../repositories/questions';
+import quizData from '../repositories/questions.json';
 import { randomInt } from '../utils/numbers';
 // import { QuizContent } from '../interface';
 import * as fs from 'fs';
@@ -33,29 +33,5 @@ export function getQuestionById(req: Request, res: Response, next: NextFunction)
 		const id = parseInt(req.params.id);
 		const questionText = JSON.parse(data)["questions"][id-1]["question"];
 		res.end(JSON.stringify(questionText));
-	})
-}
-
-export function getRandomQuestion(req: Request, res: Response, next: NextFunction) {
-	fs.readFile( __dirname + "/../repositories/questions.json", 'utf-8', function (err, data) {
-		if (err) {
-      return next(err);
-    }
-    
-    const questions = JSON.parse(data)["questions"];
-    const id = randomInt(1, questions.length); // Ensure the max is within array bounds
-    const questionText = questions[id - 1]?.question; // Use optional chaining to avoid undefined issues
-
-    if (!questionText) {
-      return res.status(404).json({ error: "Question not found" });
-    }
-
-		questionsSeen.push(id);
-
-		res.end(`
-			<p>${JSON.stringify(questionText)}</p>
-			<br/>
-			<p>You have ${questionsAll - questionsSeen.length} unseen questions</p>
-		`);
 	})
 }
--- src/routes/testRoutes.ts
+++ src/routes/testRoutes.ts
@@ -7,6 +7,5 @@ router.get('/json', testController.getList);
 router.get('/hello', testController.getHello);
 router.get('/data', testController.getData);
 router.get('/:id', testController.getQuestionById);
-router.get('/random', testController.getRandomQuestion);
 
 export default router;
