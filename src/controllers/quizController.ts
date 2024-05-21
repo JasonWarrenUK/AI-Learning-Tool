7094b610d7c75ef5f38a52f6abdaa6a251ac4fec
Jason Warren <contact.jwarren@gmail.com>
2024-05-21 15:20:11 +0100

    getRandom() correctly detects max q index

--- src/controllers/quizController.ts
+++ src/controllers/quizController.ts
@@ -3,27 +3,44 @@ import quizData from '../repositories/questions.json';
 import { QuizContent } from '../interface';
 import * as fs from 'fs';
 
-function getDefault(req: Request, res: Response, next: NextFunction) {
-	let quizContent/* : QuizContent */ = quizData/*  as QuizContent */;
-	let firstQuestion = quizContent.questions[0];
-  let htmlResponse = `<h1>${firstQuestion.question}</h1>`;
+/* Quiz Functions */
 
-  // Adding a form for the options
-  htmlResponse += `<form action="/answer" method="post">`;
+function getDefault(req: Request, res: Response, next: NextFunction){
+	const quizContent= quizData;
+  const firstQuestion = quizContent.questions[0];
   
+  let htmlResponse = `<h1>${firstQuestion.question}</h1>`;
+  htmlResponse += `<form action="/answer" method="post">`;
 	firstQuestion.options.forEach(option => {
     htmlResponse += `<input type="radio" id="${option}" name="option" value="${option}">
                      <label for="${option}">${option}</label><br>`;
   });
-
   htmlResponse += `<input type="submit" value="Submit"></form>`;
 
   res.send(htmlResponse);
 }
 
-function getRandom(){}
+function getRandom(req: Request, res: Response, next: NextFunction){
+  const quiz = quizData;
+  const qMax = quiz.questions.length - 1;
 
-/* function answer (req: Request, res: Response) {
+  console.log(`Max Q: ${qMax}`);
+
+	/* let firstQuestion = quiz.questions[0];
+
+  let htmlResponse = `<h1>${firstQuestion.question}</h1>`;
+  htmlResponse += `<form action="/answer" method="post">`;
+	firstQuestion.options.forEach(option => {
+    htmlResponse += `<input type="radio" id="${option}" name="option" value="${option}">
+                     <label for="${option}">${option}</label><br>`;
+  });
+  htmlResponse += `<input type="submit" value="Submit"></form>`; */
+
+  res.send(`getRandom finished`);
+}
+
+/*
+function answer (req: Request, res: Response) {
   const userAnswer = req.body.option;
   const correctAnswer = quizContent.quiz.questions[0].answer;
 
@@ -32,6 +49,7 @@ function getRandom(){}
   } else {
     res.send("Incorrect! The correct answer was " + correctAnswer);
   }
-}; */
+};
+*/
 
 export default { /* answer, */ getDefault, getRandom };
